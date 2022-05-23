902765b6026c286ee409b3c556c4f7b03fc03e77
Dan Ellis <daniel.ellis.research@googlemail.com>
2022-05-24 00:32:41 +0100

    auto-update

--- storytelling/density.py
+++ storytelling/density.py
@@ -4,7 +4,7 @@ from . import config
 
 
 
-for category in config.CATEGORIES:
+# for category in config.CATEGORIES:
     #
 
 #  mf = pd.read_csv(''.join([config.CSVTABLES,getattr(config,category),config.simpletable['mf'],'.csv']),index_col=0)
